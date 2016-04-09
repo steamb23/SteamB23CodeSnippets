dddbb9bc273861af6f68cad1a25edaebdc3b1705
steamb23 <steamb23@gmail.com>
2016-04-10 03:33:09 +0900

    SecureValueTypeSerializationInfo 클래스 추가

---
+++ SteamB23.Security/SecureType/SecureValueTypeSerializationInfo.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization;
+
+namespace System
+{
+    /// <summary>
+    /// <c>SerializationInfo</c>의 SecureValueType 확장을 정의합니다.
+    /// </summary>
+    public static class SecureValueTypeSerializationInfo
+    {
+        public static void AddValue(this SerializationInfo info, string name, SecureInt64 value)
+        {
+            info.AddValue(name, value, typeof(SecureInt64));
+        }
+        public static void AddValue(this SerializationInfo info, string name, SecureDouble value)
+        {
+            info.AddValue(name, value, typeof(SecureDouble));
+        }
+        public static SecureInt64 GetSecureInt64(this SerializationInfo info, string name)
+        {
+            return (SecureInt64)info.GetValue(name, typeof(SecureInt64));
+        }
+        public static SecureDouble GetSecureDouble(this SerializationInfo info, string name)
+        {
+            return (SecureDouble)info.GetValue(name, typeof(SecureDouble));
+        }
+    }
+}
