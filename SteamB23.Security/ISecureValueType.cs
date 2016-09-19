07d86075e60398bac40ef62047ad51fc2def63d6
steamb23 <steamb23@gmail.com>
2016-09-19 11:07:07 +0900

    xml 문서화 수정

--- SteamB23.Security/ISecureValueType.cs
+++ SteamB23.Security/ISecureValueType.cs
@@ -10,13 +10,13 @@
 namespace SteamB23.Security
 {
     /// <summary>
-    /// 값을 암호화하여 저장하는 메서드를 정의합니다.
+    /// 값을 보호하는 메서드를 정의합니다.
     /// </summary>
     /// <typeparam name="T">암호화되지 않은 값의 형식입니다.</typeparam>
     public interface ISecureValueType<T> where T : struct
     {
         /// <summary>
-        /// 암호화 되지 않은 값을 설정하거나 가져옵니다.
+        /// 보호되지 않은 값을 설정하거나 가져옵니다.
         /// </summary>
         T Value
         {
